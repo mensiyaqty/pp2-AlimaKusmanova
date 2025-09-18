d284aa94069cdf8eb8c1536f5193d74dbd63e09c
Temirlan Nussipkozha <xpomoy.14@gmail.com>
2025-09-18 16:20:55 +0500

    first commit

---
+++ lab1/python variables/python_variables.py
@@ -0,0 +1,33 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4
+x = "Sally"
+print(x)
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+x = "John"
+print(x)
+#double quotes are the same as single quotes:
+x = 'John'
+print(x)
+
+a = 4
+A = "Sally"
+
+print(a)
+print(A)
